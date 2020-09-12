b85a253961dbdf81f685ed5ad6bc2b4fccef2a36
Shrivatsa <shrivatsa.swadi@gmail.com>
2020-09-12 07:11:02 +0530

    Added Quarkus

---
+++ quarkus/src/test/java/org/example/NativeGreetingIT.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingIT extends GreetingTest {
+
+    // Execute the same tests but in native mode.
+}
