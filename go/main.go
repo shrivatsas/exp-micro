aa670717f39a34fcc9812081d0f7269072b8dcbf
Shrivatsa <shrivatsa.swadi@gmail.com>
2020-09-15 16:33:38 +0530

    Fixed gin/gonic opentelemetry

--- go/main.go
+++ go/main.go
@@ -1,13 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	// "html/template"
+	"flag"
+	"log"
+	// "net/http"
+
+	"github.com/gin-gonic/gin"
+	gintrace "go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin"
+	otelglobal "go.opentelemetry.io/otel/api/global"
+	// oteltrace "go.opentelemetry.io/otel/api/trace"
+	zipkin "go.opentelemetry.io/otel/exporters/trace/zipkin"
+	// "go.opentelemetry.io/otel/label"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+var tracer = otelglobal.Tracer("gin-server")
+
+func initTracer(url string) {
+	err := zipkin.InstallNewPipeline(
+		url,
+		"zipkin-test",
+		zipkin.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
+	url := flag.String("zipkin", "http://localhost:9411/api/v2/spans", "zipkin url")
+	flag.Parse()
+
+	initTracer(*url)
 	r := gin.Default()
+	r.Use(gintrace.Middleware("my-server"))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
