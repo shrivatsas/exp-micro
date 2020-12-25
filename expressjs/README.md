e195690b43b5104da82d59711c6fdaa73dc1bfea
Shrivatsa <shrivatsa.swadi@gmail.com>
2020-12-25 22:43:16 +0530

    Expressjs: random fact fetch using axios

---
+++ expressjs/README.md
@@ -0,0 +1,12 @@
+## Live reload
+npm init
+npm install livereload nodemon --save
+npm install axios --save
+
+## Fetch random text / facts
+1. From a set of third parties
+    * https://uselessfacts.jsph.pl/random.json?language=en
+    * http://quotes.stormconsultancy.co.uk/random.json
+    * https://api.whatdoestrumpthink.com/api/v1/quotes/random
+2. From a local set
+    * Scrape movie quotes to a local store
--- expressjs/main.js
+++ expressjs/main.js
@@ -1,5 +1,6 @@
-
 const express = require('express')
+const axios = require('axios');
+
 const { countAllRequests } = require("./monitoring");
 
 const app = express()
@@ -8,6 +9,17 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/random', (req, res) => {
+  axios.get('https://uselessfacts.jsph.pl/random.json?language=en')
+  .then(r => {
+    res.send(r.data.text)
+  })
+  .catch(err => {
+    console.log(err);
+    res.send('Hello errld!')
+  });
 })
 
 app.listen(port, () => {
