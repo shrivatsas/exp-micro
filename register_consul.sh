ffb41adf3efd25ec5eaa07dcaec0bfa662a7e39b
Shrivatsa <shrivatsa.swadi@gmail.com>
2021-01-04 22:50:19 +0530

    Added consul registration config and script

---
+++ register_consul.sh
@@ -0,0 +1,13 @@
+docker inspect micro-exp_express_1 --format '{{.NetworkSettings.Networks.microexp.IPAddress}}' | xargs -I '{}' sed 's/<ipaddress>/{}/' expressjs/express-consul.json | curl --request PUT --data-binary @- http://localhost:8500/v1/agent/service/register
+
+docker inspect micro-exp_fastapi_1 --format '{{.NetworkSettings.Networks.microexp.IPAddress}}' | xargs -I '{}' sed 's/<ipaddress>/{}/' fastapi/fastapi-consul.json | curl --request PUT --data-binary @- http://localhost:8500/v1/agent/service/register
+
+docker inspect micro-exp_gin_1 --format '{{.NetworkSettings.Networks.microexp.IPAddress}}' | xargs -I '{}' sed 's/<ipaddress>/{}/' go/go-consul.json | curl --request PUT --data-binary @- http://localhost:8500/v1/agent/service/register
+
+docker inspect micro-exp_quarkus_1 --format '{{.NetworkSettings.Networks.microexp.IPAddress}}' | xargs -I '{}' sed 's/<ipaddress>/{}/' quarkus/quarkus-consul.json | curl --request PUT --data-binary @- http://localhost:8500/v1/agent/service/register
+
+docker inspect micro-exp_helidon-se_1 --format '{{.NetworkSettings.Networks.microexp.IPAddress}}' | xargs -I '{}' sed 's/<ipaddress>/{}/' helidon-se/helidon-consul.json | curl --request PUT --data-binary @- http://localhost:8500/v1/agent/service/register
+
+docker inspect micro-exp_luminous_1 --format '{{.NetworkSettings.Networks.microexp.IPAddress}}' | xargs -I '{}' sed 's/<ipaddress>/{}/' luminous-clj/luminous-consul.json | curl --request PUT --data-binary @- http://localhost:8500/v1/agent/service/register
+
+docker inspect micro-exp_micronaut_1 --format '{{.NetworkSettings.Networks.microexp.IPAddress}}' | xargs -I '{}' sed 's/<ipaddress>/{}/' micronaut/micronaut-consul.json | curl --request PUT --data-binary @- http://localhost:8500/v1/agent/service/register
